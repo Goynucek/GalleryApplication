5e50e5caebbd5a365f998c90dae83a87128d8a17
Hasan Göynücek <78232847+Goynucek@users.noreply.github.com>
2024-02-26 03:43:56 +0300

    Add files via upload

---
+++ GalleryApplication/GalleryApplication/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace GalleryApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuDosya = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuDosya});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuDosya
+            // 
+            this.mnuDosya.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAc,
+            this.mnuKaydet,
+            this.toolStripMenuItem1,
+            this.mnuAdd,
+            this.toolStripMenuItem2,
+            this.mnuExit});
+            this.mnuDosya.Name = "mnuDosya";
+            this.mnuDosya.Size = new System.Drawing.Size(51, 20);
+            this.mnuDosya.Text = "Dosya";
+            // 
+            // mnuAc
+            // 
+            this.mnuAc.Name = "mnuAc";
+            this.mnuAc.Size = new System.Drawing.Size(180, 22);
+            this.mnuAc.Text = "Aç";
+            this.mnuAc.Click += new System.EventHandler(this.mnuAc_Click);
+            // 
+            // mnuKaydet
+            // 
+            this.mnuKaydet.Name = "mnuKaydet";
+            this.mnuKaydet.Size = new System.Drawing.Size(180, 22);
+            this.mnuKaydet.Text = "Kaydet";
+            this.mnuKaydet.Click += new System.EventHandler(this.mnuKaydet_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuAdd
+            // 
+            this.mnuAdd.Name = "mnuAdd";
+            this.mnuAdd.Size = new System.Drawing.Size(180, 22);
+            this.mnuAdd.Text = "Ekle";
+            this.mnuAdd.Click += new System.EventHandler(this.mnuAdd_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(180, 22);
+            this.mnuExit.Text = "Çıkış";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuDosya;
+        private System.Windows.Forms.ToolStripMenuItem mnuAc;
+        private System.Windows.Forms.ToolStripMenuItem mnuKaydet;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuAdd;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ GalleryApplication/GalleryApplication/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GalleryApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Images (*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Images(*.png)|*.png|All Files (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*jpeg;*.png;*.bmp";
+            ofd.FilterIndex = 3;
+            ofd.Multiselect = true;
+            ofd.Title = "Open Images";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] FileLocations = ofd.FileNames;
+
+                foreach (string FL in FileLocations)
+                {
+
+                    Panel pnl = new Panel();
+                    pnl.BorderStyle = BorderStyle.FixedSingle;
+                    pnl.Width = 75;
+                    pnl.Height = 75;
+
+                    PictureBox pb = new PictureBox();
+                    
+                    pb.SizeMode = PictureBoxSizeMode.Zoom;
+                    pb.ImageLocation = FL;
+                    pb.Dock = DockStyle.Fill;
+
+                    pb.Click += Pb_Click;
+
+                    flowLayoutPanel1.Controls.Add(pnl);
+                    pnl.Controls.Add(pb);
+                }
+            }
+        }
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+
+            Form frm = new Form();
+            frm.Text = "Preview";
+            frm.Height = 300;
+            frm.Width = 600;
+
+
+            PictureBox pbnew = new PictureBox();
+            pbnew.ImageLocation = pb.ImageLocation;
+            pbnew.SizeMode = pb.SizeMode;
+            pbnew.Dock = DockStyle.Fill;
+
+            frm.Controls.Add(pbnew);
+            frm.Show();
+        }
+
+        private void mnuKaydet_Click(object sender, EventArgs e)
+        {
+            List<string> kaydedilecekDosyalar = new List<string>();
+
+            foreach (Panel item in flowLayoutPanel1.Controls)
+            {
+                PictureBox picturebox = (PictureBox)item.Controls[0];
+                kaydedilecekDosyalar.Add(picturebox.ImageLocation);
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save Gallery";
+            sfd.Filter = "Save File (*.txt;)| *.txt";
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllLines(sfd.FileName, kaydedilecekDosyalar);
+                MessageBox.Show("Gallery Save");
+            }
+        }
+
+        private void mnuAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Gallery";
+            ofd.Filter = "Open File (*.txt;)| *.txt";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] dosyalar = System.IO.File.ReadAllLines(ofd.FileName);
+
+                foreach (string FL in dosyalar)
+                {
+                    Panel pnl = new Panel();
+                    pnl.BorderStyle = BorderStyle.FixedSingle;
+                    pnl.Width = 75;
+                    pnl.Height = 75;
+
+                    PictureBox pb = new PictureBox();
+
+                    pb.SizeMode = PictureBoxSizeMode.Zoom;
+                    pb.ImageLocation = FL;
+                    pb.Dock = DockStyle.Fill;
+
+                    pb.Click += Pb_Click;
+
+                    flowLayoutPanel1.Controls.Add(pnl);
+                    pnl.Controls.Add(pb);
+                }
+            }
+        }
+    }
+}
